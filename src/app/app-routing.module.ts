7f4770543506f564fda7b2828e015722afad79b2
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2021-12-05 09:52:51 -0300

    Primeiros Arquivos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BaseComponent } from './components/base/base.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '',   redirectTo: '/request', pathMatch: 'full' },
+  {
+    path: 'request',
+    component: BaseComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
